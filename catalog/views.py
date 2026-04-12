b4856050d499af2a82467553ed5446722f5c4f93
Ксения Рип <ksucha.18.94@gmail.comProfile image>
2026-04-12 22:26:27 +0300

    init commit 4

--- catalog/views.py
+++ catalog/views.py
@@ -3,13 +3,13 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render (request, 'home.html')
+    return render(request, "home.html")
 
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        message = request.POST.get('message')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        message = request.POST.get("message")
 
-        return HttpResponse(f'Спасибо, {name}! Сообщение получено.')
-    return render (request, 'contacts.html')
+        return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
+    return render(request, "contacts.html")
