befd3c6cb699dc93343f5c11412b16856e119da2
Ксения Рип <ksucha.18.94@gmail.comProfile image>
2026-04-20 00:30:29 +0300

    init commit 6

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from catalog.models import Product
 
 
 def home(request):
+    products = Product.objects.all()
+    context = {"products": products}
     return render(request, "home.html")
 
 
@@ -13,3 +16,15 @@ def contacts(request):
 
         return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
     return render(request, "contacts.html")
+
+
+def product_list (request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'product_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_detail.html', context)
