8158a9a10739b37f5f2210ebc09ed83de24895d5
Ксения Рип <ksucha.18.94@gmail.comProfile image>
2026-04-28 05:48:05 +0300

    init commit 7.5

--- blog/urls.py
+++ blog/urls.py
@@ -13,7 +13,7 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path("create/", BlogCreateView.as_view(), name="blog_create"),
-    path("", BlogListView.as_view(), name="blog_list"),
+    path("/", BlogListView.as_view(), name="blog_list"),
     path("<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
     path("update/<int:pk>/", BlogUpdateView.as_view(), name="blog_update"),
     path("delete/<int:pk>/", BlogDeleteView.as_view(), name="blog_delete"),
--- catalog/urls.py
+++ catalog/urls.py
@@ -12,6 +12,6 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("home/", HomeTemplateView.as_view(), name="home"),
     path("contacts/", ContactsTemplateView.as_view(), name="contacts"),
-    path("", ProductListView.as_view(), name="product_list"),
+    path("/", ProductListView.as_view(), name="product_list"),
     path("catalog/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
 ]
